b3be6cae3f5d6dfcc1b3205f3fc6c80f7a08356a
SAMYAK99 <singhsamyak199@gmail.com>
2021-08-21 00:20:41 +0530

    Added Splash Screen

---
+++ lib/views/splashscreen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'HomeScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 200),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+            height: 150,
+            width: 150,
+            child: Center(
+                child: Image.asset(
+              'assets/images/icon.png',
+              fit: BoxFit.cover,
+            )),
+          ),
+        ));
+  }
+}
